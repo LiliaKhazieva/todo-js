4c28d65b44103535c75c21120d27537605e3aca8
Lilia Khazieva <lilia002777@mail.ru>
2026-02-22 20:42:39 +0500

    #2-fix-bags

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 class Todo {
   selectors = {
-    data: "todo",
+    data: ".todo",
+    list: ".list",
+    newTaskValue: ".field__input",
+    addButton: ".add-button",
   };
   localStorageKey = "item";
   constructor() {
-    this.dataElement = document.querySelector(this.selectors.root);
+    this.dataElement = document.querySelector(".todo");
+    this.listElement = document.querySelector(".todo-list");
+    this.addButtonElement = document.querySelector(".add-button");
+    this.newTaskValueElement = document.getElementById("new-task");
+
+    this.checkboxElement = document.querySelector(".todo-item__checkbox");
     this.state = {
       items: this.getItemsFromLocalStorage(),
     };
+    this.render();
+    this.events();
   }
 
   getItemsFromLocalStorage() {
@@ -30,4 +40,73 @@ class Todo {
       JSON.stringify(this.state.items)
     );
   }
+
+  render() {
+    this.totalCount = this.state.items.length;
+    this.listElement.innerHTML = this.state.items
+      .map(
+        ({ id, title, isChecked }) => `
+       <li class="todo-item">
+          <div class="todo-content">
+            <input type="checkbox" class="todo-item__checkbox" id='${id}' ${
+          isChecked ? "checked" : ""
+        } />
+            <label class="todo-item__label">${title}</label>
+          </div>
+
+          <div class="buttons">
+            <button class="edit-button" aria-label="Edit" title="Edit"></button>
+            <button class="delete-button" aria-label="Delete" title="Detele"></button>
+          </div>
+        </li>
+    `
+      )
+      .join("");
+  }
+
+  addItem(title) {
+    this.state.items.push({
+      id: crypto?.randomUUID() ?? Date.now().toString(),
+      title,
+      isChecked: false,
+    });
+    this.setDataToLocalStorage();
+    this.render();
+  }
+
+  toggleHandler(id) {
+    this.state.items = this.state.items.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          isChecked: !item.isChecked,
+        };
+      }
+      return item;
+    });
+    this.setDataToLocalStorage();
+    this.render();
+  }
+
+  taskFormSubmit = (e) => {
+    e.preventDefault();
+
+    const newTaskItem = this.newTaskValueElement.value;
+    if (newTaskItem.trim().length > 0) {
+      this.addItem(newTaskItem);
+      this.newTaskValueElement.value = "";
+    }
+  };
+
+  onChange = ({ target }) => {
+    if (target.matches(this.checkboxElement)) {
+      this.toggleHandler(target.id);
+    }
+  };
+
+  events() {
+    this.addButtonElement.addEventListener("click", this.taskFormSubmit);
+    this.listElement.addEventListener("change", this.toggleHandler);
+  }
 }
+new Todo();
